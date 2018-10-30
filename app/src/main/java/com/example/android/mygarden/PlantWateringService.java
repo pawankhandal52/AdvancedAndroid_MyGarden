01a64105b70b0a6d8feae9e3f340af75bf1739e1
Pawan <pksharama@stemdot.com>
2018-10-30 20:29:55 +0530

    Update app Widgets

--- app/src/main/java/com/example/android/mygarden/PlantWateringService.java
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -17,11 +17,13 @@ package com.example.android.mygarden;
 */
 
 import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
-import android.provider.Settings;
 
 import com.example.android.mygarden.provider.PlantContract;
 import com.example.android.mygarden.utils.PlantUtils;
@@ -37,6 +39,7 @@ public class PlantWateringService extends IntentService {
 
     public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
     // TODO (3): Create a new action ACTION_UPDATE_PLANT_WIDGETS to handle updating widget UI and
+    public static final String ACTION_UPDATE_PLANT_WIDGETS = "com.example.android.mygarden.action.update_plant_widget";
     // implement handleActionUpdatePlantWidgets to query the plant closest to dying and call
     // updatePlantWidgets to refresh widgets
 
@@ -56,6 +59,12 @@ public class PlantWateringService extends IntentService {
         intent.setAction(ACTION_WATER_PLANTS);
         context.startService(intent);
     }
+    
+    public static void startActionUpdatePlantWidget(Context context){
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_UPDATE_PLANT_WIDGETS);
+        context.startService(intent);
+    }
 
     /**
      * @param intent
@@ -66,10 +75,42 @@ public class PlantWateringService extends IntentService {
             final String action = intent.getAction();
             if (ACTION_WATER_PLANTS.equals(action)) {
                 handleActionWaterPlants();
+            }else if(ACTION_UPDATE_PLANT_WIDGETS.equals(action)){
+                handleActionUpdatePlantWidget();
             }
         }
     }
-
+    
+    private void handleActionUpdatePlantWidget() {
+        Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        Cursor cursor = getContentResolver().query(PLANT_URI
+        ,null
+        ,null
+        ,null
+        ,PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+        
+        
+        int imgRes = R.drawable.grass;
+        if (cursor != null && cursor.getCount()>0) {
+            cursor.moveToFirst();
+            int createTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
+            int waterTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+            int plantTypeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
+            
+            long timeNow = System.currentTimeMillis();
+            long waterAt = cursor.getLong(waterTimeIndex);
+            long createdAt = cursor.getLong(createTimeIndex);
+            int plantType = cursor.getInt(plantTypeIndex);
+            cursor.close();
+            
+            imgRes = PlantUtils.getPlantImageRes(this,timeNow-createdAt,timeNow-waterAt,plantType);
+        }
+    
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int [] appIds =  appWidgetManager.getAppWidgetIds(new ComponentName(this,PlantWidgetProvider.class));
+        PlantWidgetProvider.updatePlantWidgets(this,appWidgetManager,imgRes,appIds);
+    }
+    
     /**
      * Handle action WaterPlant in the provided background thread with the provided
      * parameters.
--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -29,7 +29,7 @@ public class PlantWidgetProvider extends AppWidgetProvider {
 
     // TODO (1): Modify updateAppWidget method to take an image recourse and call
     // setImageViewResource to update the widget’s image
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int img,
                                 int appWidgetId) {
 
         // Create an Intent to launch MainActivity when clicked
@@ -38,7 +38,7 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
         // Widgets allow click handlers to only launch pending intents
-        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+        views.setOnClickPendingIntent(img, pendingIntent);
         // Add the wateringservice click handler
         Intent wateringIntent = new Intent(context, PlantWateringService.class);
         wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
@@ -52,12 +52,17 @@ public class PlantWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // TODO (2): Move the updateAppWidget loop to a new method called updatePlantWidgets and pass through the image recourse
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+        
         // TODO (4): Call startActionUpdatePlantWidgets in onUpdate as well as in AddPlantActivity and PlantDetailActivity (add and delete plants)
+        PlantWateringService.startActionUpdatePlantWidget(context);
     }
 
+    public static void updatePlantWidgets(Context context, AppWidgetManager appWidgetManager, int img,
+                                          int [] appWidgetIds){
+        for (int appWidget:appWidgetIds) {
+                updateAppWidget(context,appWidgetManager,img,appWidget);
+        }
+    }
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         // Perform any action when one or more AppWidget instances have been deleted
